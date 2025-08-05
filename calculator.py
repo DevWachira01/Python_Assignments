5418d9c1e5074d4e62ceb55ce0ee6f659b10fe3f
DevWachira01 <iwachiradenny@gmail.com>
2025-08-05 10:27:48 +0300

    made changes to the code

--- calculator.py
+++ calculator.py
@@ -1,15 +1,25 @@
 num1= int(input("Enter your first number: ")) #prompts the user to enter first number
 num2= int(input("Enter your Second number: ")) #prompts the user to enter second number
+opera= input("Enter an operation(*,+,-,/)") #prompts the user to enter an operation
 
-sum= num1 + num2 #adds the two numbers and assigns the result to the variable sum
 
-multiple= num1*num2 #multiplies the two numbers and assigns the result to the variable multiple
+if opera=='*':
+    result = num1*num2
+    print("the answer is: ",result)
 
-division = num1/num2 #divides the two numbers and assigns the result to the variable division
+elif opera=="+":
+    result = num1 + num2
+    print("the answer is: ",result)
+elif opera=="-":
+    result = num1 -num2
+    print("the answer is: ",result)
+elif opera=="/":
+    if num2 !=0 :
+      result = num1/num2
+      print("the answer is: ",result)
+    else :print("invalid operation you cannot divide by zero")
 
-substraction =num1-num2 #Substracts the two numbers and assigns the result to the variable substraction
+else: print("Error,:Invalid operation (Choose a valid operation:+,-,/,*)")
 
-print("The sum of the 2 numbers is: " ,sum)
-print("The Multiplication of the 2 numbers is: " ,multiple)
-print("The Division of the 2 numbers is: " ,division)
-print("The Substraction of the 2 numbers is: " ,substraction)
+
+
