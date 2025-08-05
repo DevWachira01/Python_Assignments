91fa5c47fefee7d4f2f6ca4824296be5fa80eb7a
DevWachira01 <iwachiradenny@gmail.com>
2025-08-05 09:50:29 +0300

    adds a new file Calculator.py

---
+++ calculator.py
@@ -0,0 +1,15 @@
+num1= int(input("Enter your first number: ")) #prompts the user to enter first number
+num2= int(input("Enter your Second number: ")) #prompts the user to enter second number
+
+sum= num1 + num2 #adds the two numbers and assigns the result to the variable sum
+
+multiple= num1*num2 #multiplies the two numbers and assigns the result to the variable multiple
+
+division = num1/num2 #divides the two numbers and assigns the result to the variable division
+
+substraction =num1-num2 #Substracts the two numbers and assigns the result to the variable substraction
+
+print("The sum of the 2 numbers is: " ,sum)
+print("The Multiplication of the 2 numbers is: " ,multiple)
+print("The Division of the 2 numbers is: " ,division)
+print("The Substraction of the 2 numbers is: " ,substraction)
